184038a65e93d57fbf3ffae23180a24b7721caf3
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2014-11-04 21:53:52 +1100

    P2/E4: Views in depth part 1 complete

---
+++ app/controllers/main_view_controller.rb
@@ -0,0 +1,29 @@
+class MainViewController < UIViewController
+  def loadView
+    self.view = TasksView.new
+    view.button.addTarget(self, action: :add_task, forControlEvents: UIControlEventTouchUpInside)
+  end
+
+  def viewDidLoad
+    self.title = "Tasks"
+    @task = Task.new
+  end
+
+  def viewWillAppear(animated)
+    view.label.text = @task.title
+  end
+
+  def add_task
+    UIAlertView.alloc.initWithTitle(
+      "Hello World!",
+      message: "I hope you have a nice day.",
+      delegate: nil,
+      cancelButtonTitle: "NO!",
+      otherButtonTitles: "Thank you", nil
+    ).show
+
+    #vc = AddTaskViewController.new
+    #vc.task = @task
+    #navigationController.pushViewController(vc, animated: true)
+  end
+end
---
+++ app/views/tasks_view.rb
@@ -0,0 +1,29 @@
+class TasksView < UIView
+  attr_reader :button, :label
+
+  def init
+    super
+
+    self.backgroundColor = UIColor.whiteColor
+
+    @button = UIButton.new
+    @button.setTitle("Add Task", forState: UIControlStateNormal)
+    @button.setTitleColor(UIColor.blueColor, forState: UIControlStateNormal)
+    @button.frame = [[20, 100], [280, 30]]
+    addSubview(@button)
+
+    @label = UILabel.new
+    @label.frame = [[20, 150], [280, 30]]
+    addSubview(@label)
+
+    image = UIImage.imageNamed("slide")
+    image_view = UIImageView.new
+    image_view.frame = [[50, 200], [220, 220]]
+    image_view.backgroundColor = UIColor.lightGrayColor
+    image_view.image = image
+    image_view.contentMode = UIViewContentModeScaleAspectFit
+    addSubview(image_view)
+
+    self
+  end
+end
