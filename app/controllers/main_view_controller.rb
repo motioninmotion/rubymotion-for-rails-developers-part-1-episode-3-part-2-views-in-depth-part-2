54fc1e33eafdefeece47296456382fe3ff04491e
Derek Melchin <derek@wndx.com>
2018-08-16 17:21:19 -0600

    Update rake & Rakefile, remove unused params, replace deprecated code

--- app/controllers/main_view_controller.rb
+++ app/controllers/main_view_controller.rb
@@ -9,18 +9,19 @@ class MainViewController < UIViewController
     @task = Task.new
   end
 
-  def viewWillAppear(animated)
+  def viewWillAppear(_)
     view.label.text = @task.title
   end
 
   def add_task
-    UIAlertView.alloc.initWithTitle(
-      "Hello World!",
-      message: "I hope you have a nice day.",
-      delegate: nil,
-      cancelButtonTitle: "NO!",
-      otherButtonTitles: "Thank you", nil
-    ).show
+    alert = UIAlertController.alertControllerWithTitle('Hello World!',
+                                                       message: 'I hope you have a nice day.',
+                                                       preferredStyle: UIAlertControllerStyleAlert)
+    no     = UIAlertAction.actionWithTitle('NO!',       style: UIAlertActionStyleCancel,  handler: nil)
+    thanks = UIAlertAction.actionWithTitle('Thank you', style: UIAlertActionStyleDefault, handler: nil)
+    alert.addAction(no)
+    alert.addAction(thanks)
+    self.presentViewController(alert, animated: true, completion: nil)
 
     #vc = AddTaskViewController.new
     #vc.task = @task
